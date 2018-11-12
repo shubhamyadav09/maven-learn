1bf8874d2cb46a62c7b794ce978b02cc44dd5cbd
shubhamyadav09 <shubhamyadav09@outlook.com>
2018-11-12 14:09:47 +0530

    Added application.java and POM modifications

---
+++ src/main/java/school/test/Application.java
@@ -0,0 +1,10 @@
+package school.test;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the output from Application java class.");
+	}
+
+}
