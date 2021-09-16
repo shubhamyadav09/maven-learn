8097dc368fbb106c772ceb36bb5d546e67293540
Shubham Yadav <44876177+shubhamyadav09@users.noreply.github.com>
2021-09-16 18:11:55 +0530

    dummy commit

--- src/main/java/school/test/Application.java
+++ src/main/java/school/test/Application.java
@@ -3,7 +3,7 @@ package school.test;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub to start spring boot
 		System.out.println("This is the output from Application java class.");
 	}
 
